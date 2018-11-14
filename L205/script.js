57952fd86df90f06b6f96795994e9deb47718172
LumixGE <shotoo1@live.com>
2018-11-14 12:05:49 +0400

    lab2

---
+++ L205/script.js
@@ -0,0 +1,13 @@
+for (var i = 2; i < 20; i++) {
+    var isPrime = 1;
+    for (p = 2; p < i; p++) {
+        if (i % p == 0) {
+            isPrime = 0;
+        }
+    }
+
+    if(isPrime==1){
+        alert(i);
+    }
+
+}
